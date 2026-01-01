abd3efba75d1ce3804d29300718acb65c0ff3055
sakibullah2006 <sakibullah.dev@gmail.com>
2026-01-01 18:03:42 +0600

    feat: update announcement bars with new offers and discounts
    refactor: enhance country localization snippet for better filtering and search functionality
    
    refactor: improve language localization snippet for better accessibility and usability
    
    style: update social icons for improved layout and accessibility
    
    style: add CSS variables for better theming and customization options
    
    feat: implement slider component with autoplay and keyboard navigation support
    
    style: add marquee animation and utility classes in app.css

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
-import '../css/app.css';
-import Alpine from 'alpinejs';
+import "../css/app.css";
+import Alpine from "alpinejs";
+import slider from "./slider.js";
 
 window.Alpine = Alpine;
+Alpine.data("slider", slider);
 Alpine.start();
